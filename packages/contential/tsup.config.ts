2e7cf1c4c33c78e18e6da8537e993ab433bb8bf6
Marcel Thomas <marcelthomas5@gmail.com>
2023-07-18 10:42:01 +0100

    Fixed subpaths

--- packages/contential/tsup.config.ts
+++ packages/contential/tsup.config.ts
@@ -1,9 +1,10 @@
 import type { Options } from 'tsup';
 
 const options: Options = {
-  entryPoints: ['src/index.ts', 'src/chat/index.ts', 'src/prompt/index.ts'],
+  entryPoints: ['src/index.ts', 'src/chat.ts', 'src/prompt.ts'],
   format: ['cjs', 'esm'],
   dts: true,
+  outDir: 'dist',
 };
 
 export default options;
